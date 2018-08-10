ce1d8226d50a9aae1986cf536af5113f71d8f7e3
Karan Gujral <karan@prontoitlabs.com>
2018-08-10 18:46:30 +0530

    added equals hashcode in trade history

--- src/main/java/com/cf/client/poloniex/wss/model/PoloniexTrade.java
+++ src/main/java/com/cf/client/poloniex/wss/model/PoloniexTrade.java
@@ -86,4 +86,29 @@ public class PoloniexTrade {
         + price + ", amount=" + amount + ", date=" + date + "]";
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((tradeId == null) ? 0 : tradeId.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    PoloniexTrade other = (PoloniexTrade) obj;
+    if (tradeId == null) {
+      if (other.tradeId != null)
+        return false;
+    } else if (!tradeId.equals(other.tradeId))
+      return false;
+    return true;
+  }
+
 }
